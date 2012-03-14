621e417deaeb8c08ffa515af77c75b62fc429a0c
Carson Gross <carsongross@gmail.com>
2012-03-13 23:11:08 -0700

    A chrome extension to close reddit if you try to open it

---
+++ README.txt
@@ -0,0 +1,1 @@
+A chrome extension that automatically closes reddit if you try to open it
---
+++ no_reddit.js
@@ -0,0 +1,6 @@
+// ==UserScript==
+// @name          
+// @require       http://ajax.googleapis.com/ajax/libs/jquery/1.3.2/jquery.min.js
+// ==/UserScript==
+
+chrome.extension.sendRequest({}, function(response) {});
